5c32faabc0d3601bb399599f420dc6af9c57d6fa
Ankesh1A <ankeshbarahdiya05112002@gmail.com>
2025-05-20 12:03:28 +0530

    second

--- src/instant/axios.js
+++ src/instant/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
- baseURL: 'http://localhost:8080', // Backend URL
+ baseURL: 'https://avn-backend.onrender.com', // Backend URL
   withCredentials: true, // Cookies ko bhejne ke liye
 });
 
